ba2da4ed326b5545e79a21fe8b69f0f53d5c841e
rav214 <rafal1behrendt@gmail.com>
2019-10-26 17:03:08 +0200

    moved list 2 to another folder (bug), finished list 2

---
+++ Lista2_0/Lista2_0/CONST_H.h
@@ -0,0 +1,3 @@
+#pragma once
+const std::string S_NAME = "CTable";
+const int DEFAULT_TABLE_LENGTH = 5;
---
+++ Lista2_0/Lista2_0/Lista2_0.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include "CONST_H.h"
+#include "CLASS_H.h"
+
+
+void v_mod_tab(CTable *pcTab, int iNewSize) {
+	pcTab->vSetName("v_mod_pointer_modified");
+	pcTab->bSetNewSize(iNewSize);
+}
+
+void v_mod_tab(CTable cTab, int iNewSize) {
+	cTab.vSetName("v_mod_modified");
+	cTab.bSetNewSize(iNewSize);
+}
+
+int main() {
+
+	CTable tab;
+	CTable * pc_new_tab;
+	pc_new_tab = tab.pcClone();
+
+	tab.writeTable();
+
+	pc_new_tab->bSetNewSize(2);
+
+	pc_new_tab->writeTable();
+	tab.writeTable();
+
+	delete pc_new_tab;
+
+	CTable * tab1 = new CTable();
+	CTable * tab2 = new CTable("nowaNazwa", 7);
+
+	v_mod_tab(tab2, 3);
+	v_mod_tab(tab, 1);
+
+	tab.writeTable();
+	tab2->writeTable();
+
+	CTable * tab3 = new CTable(tab);
+
+	std::cout << tab1->vGetName() << std::endl;
+	std::cout << tab2->vGetName() << std::endl;
+	std::cout << tab3->vGetName() << std::endl;
+
+	delete tab1;
+	delete tab2;
+	delete tab3;
+
+}
